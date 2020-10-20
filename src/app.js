374697a646f94b860f9e570d91c9123935427b70
gmrz <gmrz@gmrzdeMacBook-Air.local>
2020-10-20 16:57:46 +0800

    tijiao

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ const chalk = require('chalk')
 const conf = require('./config/defaultconfig');
 const serve = http.createServer((req,res)=>{
     res.statusCode = 200
-    res.setHeader('Content-Type','text/plain')
-    res.end('hllow http')
+    res.setHeader('Content-Type','text/html')
+    res.write('<html>')
+    res.write('<body>')
+    res.write('hllow http!')
+    res.write('</body>')
+    res.write('</html>')
+    res.end()
 })
 serve.listen(conf.post,conf.hostname,()=>{
     const addr = `http://${conf.hostname}:${conf.post}`;
     console.info(`server started at ${chalk.green(addr)}`)
-    console.log('xxx');
 })
