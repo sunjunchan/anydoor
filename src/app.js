05cd9f38b6275d858ce59c5d0252b7ae4bd06468
sunjunchan <3238279604@qq.com>
2020-10-21 18:16:31 +0800

    tijiao

--- src/app.js
+++ src/app.js
@@ -2,34 +2,59 @@ const http = require('http');
 const conf = require('./config/defaultconfig');
 const path = require('path')
 const fs = require('fs')
+const promisify = require('util').promisify
+const stat = promisify(fs.stat)
+const readdir = promisify(fs.readdir)
 const serve = http.createServer((req,res)=>{
-    const url = req.url
-    const filePath = path.join(conf.hostname,url)
-    fs.state(filePath,(err,state)=>{
-        if(err){
-            res.statusCode = 404
-            res.setHeader('Content-Type','text/plain')
-            res.end(`${filePath} is not undefind`)
-        }
-        if(state.isFile()){//文件
+const url = req.url
+const filePath = path.join(conf.root,url)
+    try{
+        const stats = await stat(filePath)
+        if(stats.isFile()){//文件
             res.statusCode = 200
             res.setHeader('Content-Type','text/plain')
-            res.end(`${fs.createReadStream(filePath).pipe(res)}`)
+            fs.createReadStream(filePath).pipe(res)
             //异步读取文件
             // fs.createReadStream(filePath,(err,data)=>{
             //     res.end(data)
             // })
-        }else if(states.isDirectory()){//文件夹
-            fs.readdir(filePath,(err,files)=>{
+        }else if(stats.isDirectory()){//文件夹
+            try{
+                const read = readdir(filePath)
                 res.statusCode = 200
                 res.setHeader('Content-Type','text/plain')
                 res.end(files.join(','))
-            })
+            }catch(ex){
+                console.log(ex)
+            }
+            // fs.readdir(filePath,(err,files)=>{
+            //     res.statusCode = 200
+            //     res.setHeader('Content-Type','text/plain')
+            //     res.end(files.join(','))
+            // })
         }
-    })
-    res.statusCode = 200
-    res.setHeader('Content-Type','text/html')
-    res.end(filePath)
+    }catch(ex){
+        res.statusCode = 404
+        res.setHeader('Content-Type','text/plain')
+        res.end(`${filePath} is not undefind`)
+    }
+    // fs.stat(filePath,(err,stats)=>{
+    //     if(stats.isFile()){//文件
+    //         res.statusCode = 200
+    //         res.setHeader('Content-Type','text/plain')
+    //         fs.createReadStream(filePath).pipe(res)
+    //         //异步读取文件
+    //         // fs.createReadStream(filePath,(err,data)=>{
+    //         //     res.end(data)
+    //         // })
+    //     }else if(stats.isDirectory()){//文件夹
+    //         fs.readdir(filePath,(err,files)=>{
+    //             res.statusCode = 200
+    //             res.setHeader('Content-Type','text/plain')
+    //             res.end(files.join(','))
+    //         })
+    //     }
+    // })
 })
 serve.listen(conf.post,conf.hostname,()=>{
     const addr = `http://${conf.hostname}:${conf.post}`;
--- src/config/defaultconfig.js
+++ src/config/defaultconfig.js
@@ -1,4 +1,5 @@
 module.exports={
     hostname:'127.0.0.1',
-    post:9527
+    post:9527,
+    root:process.cwd()//用户当前的文件夹
 }
