8f40ed139ef0e80f8ef10e8e480c1edbd5945524
sjc <3238279604@qq.com>
2020-10-22 23:23:00 +0800

    tijiao

--- src/app.js
+++ src/app.js
@@ -2,29 +2,36 @@ const http = require('http');
 const conf = require('./config/defaultconfig');
 const path = require('path')
 const route = require('./helper/route')
-const serve = http.createServer((req,res)=>{
-const url = req.url
-const filePath = path.join(conf.root,url)
-    route(req,res,filePath);
-    // fs.stat(filePath,(err,stats)=>{
-    //     if(stats.isFile()){//文件
-    //         res.statusCode = 200
-    //         res.setHeader('Content-Type','text/plain')
-    //         fs.createReadStream(filePath).pipe(res)
-    //         //异步读取文件
-    //         // fs.createReadStream(filePath,(err,data)=>{
-    //         //     res.end(data)
-    //         // })
-    //     }else if(stats.isDirectory()){//文件夹
-    //         fs.readdir(filePath,(err,files)=>{
-    //             res.statusCode = 200
-    //             res.setHeader('Content-Type','text/plain')
-    //             res.end(files.join(','))
-    //         })
-    //     }
-    // })
-})
-serve.listen(conf.post,conf.hostname,()=>{
-    const addr = `http://${conf.hostname}:${conf.post}`;
-    console.log(addr)
-})
+class Server {
+    constructor(config){
+        this.conf = Object.assign({},conf,config)
+    }
+    start(){
+        const serve = http.createServer((req,res)=>{
+        const url = req.url
+        const filePath = path.join(conf.root,url)
+            route(req,res,filePath);
+            // fs.stat(filePath,(err,stats)=>{
+            //     if(stats.isFile()){//文件
+            //         res.statusCode = 200
+            //         res.setHeader('Content-Type','text/plain')
+            //         fs.createReadStream(filePath).pipe(res)
+            //         //异步读取文件
+            //         // fs.createReadStream(filePath,(err,data)=>{
+            //         //     res.end(data)
+            //         // })
+            //     }else if(stats.isDirectory()){//文件夹
+            //         fs.readdir(filePath,(err,files)=>{
+            //             res.statusCode = 200
+            //             res.setHeader('Content-Type','text/plain')
+            //             res.end(files.join(','))
+            //         })
+            //     }
+            // })
+        })
+        serve.listen(conf.post,conf.hostname,()=>{
+            const addr = `http://${conf.hostname}:${conf.post}`;
+            console.log(addr)
+        })
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 // process.argv 读到命令行上的工具
+const yargs =  require('yargs')
+const argv = yargs
+    .usage('anywhere [options]')
+    .option('p',{
+        alias:'port',
+        describe:'端口号',//描述
+        default:9527, //默认
+    })
+    .option('h',{
+        alias:'hostname',
+        describe:'host' 
+    })
+    .option('d',{
+        alias:'root',
+        describe:'root' 
+    })
+    .version()
+    .alias('v','version')
+    .help()
+    .argv
